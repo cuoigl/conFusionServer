8851cca4a91ecf88d6a89638836ed73c207f02d8
cuoigl <cuoi.dev@gmail.com>
2023-02-22 09:50:08 +0700

    Passport

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ const Dishes = require("./models/dishes");
 const Promotions = require("./models/promotions");
 const Leaders = require("./models/leaders");
 
+var passport = require("passport");
+var authenticate = require("./authenticate");
+
 const url = "mongodb://localhost:27017/conFusion";
 mongoose.set("strictQuery", false);
 const connect = mongoose.connect(url);
@@ -54,27 +57,23 @@ app.use(
   })
 );
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 
 function auth(req, res, next) {
-  console.log(req.session);
+  console.log(req.user);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error("You are not authenticated!");
     err.status = 403;
-    return next(err);
+    next(err);
   } else {
-    if (req.session.user === "authenticated") {
-      next();
-    } else {
-      var err = new Error("You are not authenticated!");
-      err.status = 403;
-      return next(err);
-    }
+    next();
   }
 }
-
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, "public")));
