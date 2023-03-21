26474ce7191a185031e6bcc9cd0f0f657a1b9de4
cuoigl <cuoi.dev@gmail.com>
2023-03-21 23:26:05 +0700

    Asignment 4

--- app.js
+++ app.js
@@ -4,27 +4,20 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var session = require("express-session");
-var FileStore = require("session-file-store")(session);
-
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var dishRouter = require("./routes/dishRouter");
 var promoRouter = require("./routes/promotionRouter");
 var leaderRouter = require("./routes/leaderRouter");
+var favoriteRouter = require("./routes/favoriteRouter");
 const uploadRouter = require("./routes/uploadRouter");
 
 const mongoose = require("mongoose");
 
 var config = require("./config");
 
-const Dishes = require("./models/dishes");
-const Promotions = require("./models/promotions");
-const Leaders = require("./models/leaders");
-
 var passport = require("passport");
-var authenticate = require("./authenticate");
 
 const url = config.mongoUrl;
 mongoose.set("strictQuery", false);
@@ -81,6 +74,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
+app.use("/favorites", favoriteRouter);
 app.use("/imageUpload", uploadRouter);
 
 // catch 404 and forward to error handler
